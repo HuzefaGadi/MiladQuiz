428f17e51245cd82110b743bee45e136e0fab40e
huzefagadi <learndroid53@gmail.com>
2016-01-24 23:25:02 +0530

    -final changes

--- app/src/main/java/com/saiflimited/miladquiz/Constants.java
+++ app/src/main/java/com/saiflimited/miladquiz/Constants.java
@@ -5,13 +5,20 @@ package com.saiflimited.miladquiz;
  */
 public class Constants {
 
-    public static String ACTIVE_QUESTION_URL = "http://statigsoft.com/QuizBuzz/activequestion.php";
+    /*public static String ACTIVE_QUESTION_URL = "http://statigsoft.com/QuizBuzz/activequestion.php";
     public static String LOGIN_URL = "http://statigsoft.com/QuizBuzz/login.php";
-    public static String ANSWER_URL = "http://statigsoft.com/QuizBuzz/submitanswer.php";
+    public static String ANSWER_URL = "http://statigsoft.com/QuizBuzz/submitanswer.php";*/
+
+
+    public static String ACTIVE_QUESTION_URL = "http://192.168.3.2/activequestion.php";
+    public static String LOGIN_URL = "http://192.168.3.2/login.php";
+    public static String ANSWER_URL = "http://192.168.3.2/submitanswer.php";
 
     public static String USERNAME = "username";
     public static String PREFERENCE_NAME = "QUizApp";
     public static String CURRENT_QUESTION_NUMBER = "currentQuestionNumber";
+
+    public static final int hearbeat = 1000*20;
 
 
 
--- app/src/main/java/com/saiflimited/miladquiz/QuizQuestionActivity.java
+++ app/src/main/java/com/saiflimited/miladquiz/QuizQuestionActivity.java
@@ -20,7 +20,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Timer;
@@ -35,27 +38,60 @@ public class QuizQuestionActivity extends AppCompatActivity {
     String currentQuestionNumber;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
-    RelativeLayout questionLayout, waitingLayout;
-    TextView previousQuestionAnswer;
+    RelativeLayout questionLayout, waitingLayout, finishLayout;
+    TextView previousQuestionAnswer, welcomeMessage;
     Timer timer;
     TimerTask timertask;
     ProgressDialog progressDialogForActiveQuestion;
+
+    public void stopTimer()
+    {
+        try
+        {
+            timer.cancel();
+            timertask.cancel();
+        }catch (Exception e)
+        {
+
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+       stopTimer();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        timer = new Timer();
+        timertask = new TimerTask() {
+            @Override
+            public void run() {
+                callWebServiceUtilityForActiveQuestion();
+            }
+        };
+        timer.schedule(timertask, 100, Constants.hearbeat);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_question);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         sharedPreferences = getSharedPreferences(Constants.PREFERENCE_NAME, MODE_PRIVATE);
         editor = sharedPreferences.edit();
         questionLayout = (RelativeLayout) findViewById(R.id.layout_question);
         waitingLayout = (RelativeLayout) findViewById(R.id.layout_waitingscreen);
+        finishLayout = (RelativeLayout) findViewById(R.id.layout_finish_screen);
         submit = (Button) findViewById(R.id.submit);
         previousQuestionAnswer = (TextView) findViewById(R.id.rightAnswerText);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         questionNumber = (TextView) findViewById(R.id.questionNumber);
         username = sharedPreferences.getString(Constants.USERNAME, "");
+        welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
+        welcomeMessage.setText("Welcome " + username);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,15 +129,9 @@ public class QuizQuestionActivity extends AppCompatActivity {
         progressDialogForActiveQuestion.setCancelable(false);
         progressDialogForActiveQuestion.show();
 
-        timer = new Timer();
-        timertask = new TimerTask() {
-            @Override
-            public void run() {
-                callWebServiceUtilityForActiveQuestion();
-            }
-        };
 
-        timer.schedule(timertask, 100, 20000);
+
+
 
     }
 
@@ -136,7 +166,11 @@ public class QuizQuestionActivity extends AppCompatActivity {
             } catch (MalformedURLException e) {
                 e.printStackTrace();
 
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
             } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
@@ -165,14 +199,24 @@ public class QuizQuestionActivity extends AppCompatActivity {
                 if (result != null) {
 
                     if (result.equalsIgnoreCase("TRUE")) {
-                        Toast.makeText(QuizQuestionActivity.this, "ANSWER SAVED SUCCESSFULLY", Toast.LENGTH_LONG).show();
+                        Toast.makeText(QuizQuestionActivity.this, "Answered saved succesfully", Toast.LENGTH_LONG).show();
                         waitingLayout.setVisibility(View.VISIBLE);
                         questionLayout.setVisibility(View.GONE);
+                        radioGroup.clearCheck();
+                    } else if (result.equalsIgnoreCase("FALSE")) {
+                        Toast.makeText(QuizQuestionActivity.this, "You have Already answered this question", Toast.LENGTH_LONG).show();
+                        waitingLayout.setVisibility(View.VISIBLE);
+                        questionLayout.setVisibility(View.GONE);
+                        radioGroup.clearCheck();
                     } else {
-                        Toast.makeText(QuizQuestionActivity.this, "ISSUE WITH SERVER, TRY AGAIN", Toast.LENGTH_LONG).show();
+                        Toast.makeText(QuizQuestionActivity.this, "Issue with server, Try again", Toast.LENGTH_LONG).show();
                         waitingLayout.setVisibility(View.GONE);
                         questionLayout.setVisibility(View.VISIBLE);
                     }
+                } else {
+                    Toast.makeText(QuizQuestionActivity.this, "Issue with server, Try again", Toast.LENGTH_LONG).show();
+                    waitingLayout.setVisibility(View.GONE);
+                    questionLayout.setVisibility(View.VISIBLE);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -202,10 +246,17 @@ public class QuizQuestionActivity extends AppCompatActivity {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
                 String jsonResultLine = reader.readLine();
                 return jsonResultLine;
+            } catch (SocketTimeoutException e) {
+                Toast.makeText(getApplicationContext(), "Server Not Responding", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
             } catch (MalformedURLException e) {
                 e.printStackTrace();
 
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
             } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
@@ -226,23 +277,34 @@ public class QuizQuestionActivity extends AppCompatActivity {
                     progressDialogForActiveQuestion.hide();
                 }
                 if (result != null) {
-                    currentQuestionNumber = result;
-                    System.out.println("Answer Current " + currentQuestionNumber);
-                    String currentQuestionNumberFromPreferences = sharedPreferences.getString(Constants.CURRENT_QUESTION_NUMBER, "0");
-                    questionNumber.setText(currentQuestionNumber);
-                    if (!currentQuestionNumberFromPreferences.equalsIgnoreCase(currentQuestionNumber)) {
-                        editor.putString(Constants.CURRENT_QUESTION_NUMBER, currentQuestionNumber).commit();
+                    if (result.equalsIgnoreCase("P")) {
+                        waitingLayout.setVisibility(View.VISIBLE);
+                        questionLayout.setVisibility(View.GONE);
+                        finishLayout.setVisibility(View.GONE);
 
-                        if (!currentQuestionNumber.equalsIgnoreCase("1")) {
-                            waitingLayout.setVisibility(View.VISIBLE);
-                            questionLayout.setVisibility(View.GONE);
-                            showDelayedAnswer(currentQuestionNumberFromPreferences, "B");
-                        }
+                    } else if (result.equalsIgnoreCase("F")) {
+                        finishLayout.setVisibility(View.VISIBLE);
+                        waitingLayout.setVisibility(View.GONE);
+                        questionLayout.setVisibility(View.GONE);
+                        editor.clear().commit();
+                        stopTimer();
 
                     } else {
+                        currentQuestionNumber = result;
+                        System.out.println("Question Current " + currentQuestionNumber);
+                        String currentQuestionNumberFromPreferences = sharedPreferences.getString(Constants.CURRENT_QUESTION_NUMBER, "0");
+                        questionNumber.setText(currentQuestionNumber);
+                        if (!currentQuestionNumberFromPreferences.equalsIgnoreCase(currentQuestionNumber)) {
+                            editor.putString(Constants.CURRENT_QUESTION_NUMBER, currentQuestionNumber).commit();
 
+
+                                showDelayedQuestionAfterAnswer();
+
+
+                        } else {
+
+                        }
                     }
-
 
                 }
 
@@ -252,7 +314,7 @@ public class QuizQuestionActivity extends AppCompatActivity {
 
         }
 
-        public void showDelayedAnswer(final String previousQuestionNumber, final String answer) {
+       /* public void showDelayedAnswer() {
             Timer timer = new Timer();
             TimerTask timerTask = new TimerTask() {
                 @Override
@@ -260,9 +322,8 @@ public class QuizQuestionActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            previousQuestionAnswer.setText("Answer for Question No. " + previousQuestionNumber + "is " + answer);
-                            previousQuestionAnswer.setVisibility(View.VISIBLE);
-                            showDelayedQuestionAfterAnswer();
+
+
                         }
                     });
 
@@ -270,27 +331,21 @@ public class QuizQuestionActivity extends AppCompatActivity {
                 }
             };
             timer.schedule(timerTask, 30000);
-        }
+        }*/
 
-        public void showDelayedQuestionAfterAnswer()
-        {
-            Timer timer = new Timer();
-            TimerTask timerTask = new TimerTask() {
-                @Override
-                public void run() {
+        public void showDelayedQuestionAfterAnswer() {
+
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
 
                             waitingLayout.setVisibility(View.GONE);
                             questionLayout.setVisibility(View.VISIBLE);
+                            finishLayout.setVisibility(View.GONE);
                         }
                     });
 
 
-                }
-            };
-            timer.schedule(timerTask, 5000);
         }
     }
 
